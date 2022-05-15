5b4ae4f701002a0c7c100a2c9342bed102b94963
ilmir <mr.ilmirka16@mail.ru>
2022-05-15 22:00:02 +0300

    Create task001

---
+++ Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Введите число обозначающее день: ");
+            string str = Console.ReadLine();
+            int a = int.Parse(str);
+            if (a == 4 || a == 5){
+                Console.WriteLine("Данный номер дня является выходным!");
+            }
+            else
+                Console.WriteLine("Данный номер дня не является выходным!");
+        }
+    }
+}
