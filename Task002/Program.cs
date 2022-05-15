93a5c264e8cff04c71f5b66f56fc45d98f00b5c3
ilmir <mr.ilmirka16@mail.ru>
2022-05-15 22:08:43 +0300

    Create task002

---
+++ Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 1 число: ");
+            string stra = Console.ReadLine();
+            Console.Write("Введите 2 число: ");
+            string strb = Console.ReadLine();
+            int a = int.Parse(stra);
+            int b = int.Parse(strb);
+            if (a == b * b){
+                Console.WriteLine($"Число {a} является квадратом числа {b}");
+            }
+            else{
+                 Console.WriteLine($"Число {a} не является квадратом числа {b}");
+            }
+        }
+    }
+}
